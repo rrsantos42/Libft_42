0350723a2d41caef7e827b67205e959c3a7c3ae1
Ricardo Santos <anaraqsantos@gmail.com>
2022-08-30 03:39:19 +0100

    map

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,13 +6,28 @@
 /*   By: rsantos <rsantos@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/15 17:33:38 by rsantos           #+#    #+#             */
-/*   Updated: 2022/08/19 05:38:04 by rsantos          ###   ########.fr       */
+/*   Updated: 2022/08/30 03:34:16 by rsantos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
-// {
-	
-// }
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*first;
+
+	first = NULL;
+	while (lst)
+	{
+		new = ft_lstnew(f(lst->content));
+		if (!new)
+		{
+			ft_lstclear(&first, del);
+			return (0);
+		}
+		ft_lstadd_back(&first, new);
+		lst = lst->next;
+	}
+	return (first);
+}
