b1fd46eada060747b78256729351b3f80ee04970
Ricardo Santos <anaraqsantos@gmail.com>
2022-10-11 16:44:28 +0100

    minor change

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: rsantos <rsantos@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 04:58:58 by rsantos           #+#    #+#             */
-/*   Updated: 2022/08/12 05:04:15 by rsantos          ###   ########.fr       */
+/*   Updated: 2022/10/11 16:41:37 by rsantos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(const char *s1, int c, size_t n)
+void	*ft_memset(void *s1, int c, size_t n)
 {
 	char *str;
 	str = (char *)s1;
